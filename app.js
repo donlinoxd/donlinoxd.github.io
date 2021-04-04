69d42d025625289317f95e9d386ed8047383e066
donlinoxd <80018086+donlinoxd@users.noreply.github.com>
2021-04-04 13:39:26 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const burger = document.querySelector(".nav-burger");
 const navLinks = document.querySelector(".nav-links");
 const navLink = document.querySelectorAll(".nav-link");
 const header = document.querySelector("header");
+
 const lightMode = document.querySelector(".lightmode-container i");
+
 const sections = document.querySelectorAll("section");
+
 const projectBx = document.querySelectorAll(".project-box");
+
 const innerHeight = window.innerHeight;
 const innerWidth = window.innerWidth;
 
@@ -82,12 +86,7 @@ window.addEventListener("scroll", () => {
 // ----------MINHEIGHT
 sections.forEach((section) => {
   section.style.minHeight = `${innerHeight}px`;
-  if (section.classList.contains("testimonial-container")) {
-    section.style.minHeight = innerHeight * 0.8 + "px";
-  }
 });
-document.querySelector(".form-container").style.minHeight =
-  window.innerHeight * 0.75 + "px";
 
 //   ------------TYPED.JS
 var typed = new Typed("#name-title", {
@@ -97,4 +96,85 @@ var typed = new Typed("#name-title", {
   backDelay: 1000,
   loop: true,
 });
+
 // ----------TESTIMONIAL SLIDER
+const slideContainer = document.querySelector(".testimonial-slide");
+const slideContent = document.querySelectorAll(".testimonial-content");
+const slideContentWidth = slideContent[0].offsetWidth;
+const spaceBetween = 15;
+
+const radioIndex = document.querySelectorAll(".index-radio i");
+
+// for space between content
+slideContent.forEach((content) => {
+  content.style.marginRight = `${spaceBetween}px`;
+});
+
+let index = 0;
+
+const cloneFirstSlide = slideContent[0].cloneNode(true);
+const cloneLastSlide = slideContent[slideContent.length - 1].cloneNode(true);
+
+slideContainer.appendChild(cloneFirstSlide);
+slideContainer.insertBefore(cloneLastSlide, slideContent[0]);
+
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+
+next.addEventListener("click", () => switchSlide("next"));
+prev.addEventListener("click", () => switchSlide("prev"));
+
+// CLICKING ON RADIO INDEX
+for (let i = 0; radioIndex.length > i; i++) {
+  radioIndex[i].addEventListener("click", () => switchSlide(i));
+}
+
+slideContainer.addEventListener("transitionend", checkIndex);
+
+function switchSlide(arg) {
+  slideContainer.classList.add("slide-transition");
+  if (arg == "next") {
+    slideContainer.style.left = `${
+      slideContainer.offsetLeft - slideContentWidth - spaceBetween
+    }px`;
+    index++;
+  } else if (arg == "prev") {
+    slideContainer.style.left = `${
+      slideContainer.offsetLeft + slideContentWidth + spaceBetween
+    }px`;
+    index--;
+  } else {
+    let i = arg;
+    slideContainer.style.left = `-${
+      (slideContentWidth + spaceBetween) * i + 255
+    }px`;
+    index = i;
+  }
+}
+
+function checkIndex() {
+  slideContainer.classList.remove("slide-transition");
+  if (index == -1) {
+    slideContainer.style.left = `-${
+      slideContent.length * (slideContentWidth + spaceBetween)
+    }px`;
+    index = slideContent.length - 1;
+  } else if (index == slideContent.length) {
+    slideContainer.style.left = `-${slideContentWidth + spaceBetween}px`;
+    index = 0;
+  }
+  //   Radio index active
+  radioIndex.forEach((radio) => {
+    radio.classList.remove("index-active");
+  });
+  radioIndex[index].classList.add("index-active");
+
+  //   Slide content active
+  slideContent.forEach((content) => {
+    content.classList.remove("testi-active");
+  });
+  slideContent[index].classList.add("testi-active");
+
+  // remove the animation on clone node
+  cloneFirstSlide.classList.remove("testi-active");
+}
